621afcefd603327d8f1939e35ac8bf3664ddc841
Wenhao Chen <chenwenh@oregonstate.edu>
2023-02-23 17:53:51 -0800

    added API call to get manufacturer details

--- react_frontend/src/pages/ManufacturersPage.js
+++ react_frontend/src/pages/ManufacturersPage.js
@@ -3,29 +3,27 @@ import {Link} from 'react-router-dom';
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 import MfrForm from "../components/MfrForm";
 import MfrInfo from "../components/MfrInfo";
+import axios from "axios";
 
 function ManufacturersPage(){
 
     // set manufacturer selection
     const [mfrSelections, setMfrSelections] = useState([]);
     const [mfrDetails, setMfrDetails] = useState(null);
+    const [mfrWiki, setMfrWiki] = useState(null);
+
     // used to make API call
     async function getMfrDetails(mfrSelections) {
         if (mfrSelections.length > 0) {
-            const response = await fetch("/api/VINLookup", {
-                method: "POST",
-                body: JSON.stringify(payload),
-                headers: {
-                    'Content-Type': 'application/json'
-                  }
+            const response = await axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/getmanufacturerdetails/${mfrSelections[0]["Mfr_ID"]}?format=json`, {
+                method: "GET",
             });
             
             // handle the microservice response.
-            if (response.ok) {
-                const res_data = await response.json()
+            if (response.status == 200) {
+                const res_data = await response.data
                 console.log("response", res_data);
-                set_vehicle_data(res_data);
-                navigate("/decoderresults");
+                setMfrDetails(res_data);
             } else {
                 console.log("Failed to get data. Status code:", response.status);
                 alert(`Failed to get data. Status code: ${response.status}`);
@@ -34,6 +32,7 @@ function ManufacturersPage(){
     }
     useEffect(() => {
         console.log("updated to", mfrSelections)
+        getMfrDetails(mfrSelections)
     }, [mfrSelections])
 
     return (
@@ -44,6 +43,7 @@ function ManufacturersPage(){
         </Breadcrumb>
         <MfrForm mfrSelections={mfrSelections} setMfrSelections={setMfrSelections}/>
         <MfrInfo mfrSelections={mfrSelections}/>
+        <p>{mfrDetails != null ? JSON.stringify(mfrDetails) : "No details yet."}</p>
         </>
     )
 }
